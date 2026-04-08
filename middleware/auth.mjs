4f3e137da084c642048c366dee274ae4137875c4
graysenwargo <graysenwargo@gmail.com>
2026-04-08 15:18:48 -0500

    added error handler and auth placeholder middleware

---
+++ middleware/auth.mjs
@@ -0,0 +1,10 @@
+// middleware/auth.mjs
+// placeholder for authentication middleware (Milestone #3)
+// source: https://expressjs.com/en/guide/using-middleware.html
+
+// TODO: implement JWT auth check here during Auth System milestone
+function authCheck(req, res, next) {
+  next();
+}
+
+export default authCheck;
